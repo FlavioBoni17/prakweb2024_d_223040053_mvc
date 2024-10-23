f7daa0fe030b7f2222041f5c02a8dfcdc36199cd
Flavio Boni <flavioboni17@gmail.com>
2024-10-23 09:58:34 +0700

    Membuat Aplikasi MVC dengan PHP #5 View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,10 +1,22 @@
 <?php 
 
-class About {
-    public function index($nama = 'Flavio', $pekerjaan = 'Mahasiswa') {
-        echo "Halo, nama saya $nama saya seorang $pekerjaan";
+class About extends Controller {
+    public function index($nama = 'Flavio', $pekerjaan = 'Mahasiswa', $umur = 18) 
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
-    public function page() {
-        echo 'About/page';
+
+    public function page() 
+    {
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
